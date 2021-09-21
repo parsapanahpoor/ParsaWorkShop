2761bc1cba371f799bf21be5a8d0af89f5fd2753
parsapanahpoor <80142438+parsapanahpoor@users.noreply.github.com>
2021-09-21 14:46:12 +0430

    Initial Comment Table

---
+++ Domain/Models/Comment/Comment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Comment
+{
+    public class Comment
+    {
+
+        [Key]
+        public int CommentId { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public int? ProductID { get; set; }
+
+        public int? VideoId { get; set; }
+
+        public int? BlogId { get; set; }
+
+        [MaxLength(700)]
+        public string CommentBody { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public bool IsAdminRead { get; set; }
+
+        #region Relations
+
+        [ForeignKey("ParentId")]
+        public List<Comment> comments { get; set; }
+        public  ProductType ProductType { get; set; }
+        public  User Users { get; set; }
+        public  Models.Entites.Blog.Video Video { get; set; }
+        public  Models.Entites.Blog.Blog Blog { get; set; }
+        public  Models.Entites.Product.Product Product { get; set; }
+
+        #endregion
+
+    }
+}
